2fe6525210d308cfb2c51b84243105a012780fb9
Bassel <basselkanso82@gmail.com>
2019-12-16 13:56:28 +0200

    exercise 6 solved (step6)

--- Exercises/Javascript-Basics-01/Step_6.js
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -1,6 +1,6 @@
 document.getElementById("validate").addEventListener('click',()=>{
     let firstNumber = document.getElementById("first_number").value;
     let secondNumber = document.getElementById("second_number").value;
-    let result = firstNumber * secondNumber;
+    let result = firstNumber % secondNumber;
     alert(`result = ${result}`);
 });
