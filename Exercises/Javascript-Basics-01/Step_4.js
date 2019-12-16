7da2f759a0f0a7b883accc31c16313325d746237
Bassel <basselkanso82@gmail.com>
2019-12-16 12:14:53 +0200

    finished exercise 4 (step 4)

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,6 @@
+let name = prompt("Please enter your name");
+let surname = prompt("Please enter your surname");
+let city = prompt("Please enter your city");
+
+alert(` Nom : ${name} \n Prenom : ${surname} \n Ville : ${city}`);
+
