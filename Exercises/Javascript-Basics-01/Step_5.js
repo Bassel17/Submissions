a8950cbb0ea66b78750436dbd389e9e38a1f81f4
Bassel <basselkanso82@gmail.com>
2019-12-16 13:52:35 +0200

    exercise 5 solved (step 5)

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,6 @@
+document.getElementById("validate").addEventListener('click',(event)=>{
+    let firstNumber = document.getElementById("first_number").value;
+    let secondNumber = document.getElementById("second_number").value;
+    let result = firstNumber * secondNumber;
+    alert(`result = ${result}`);
+});
