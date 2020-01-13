93078e3355bfacda30a11a04880dbaa49b23b896
Bassel Kanso <basselkanso82@gmail.com>
2020-01-13 09:43:40 +0200

    Added my name to the app!

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header"></header>
-        <main className="app__main"></main>
+        <header className="app__header"><h1>Bassel Kanso</h1></header>
+        <main className="app__main"><p>The Best in the World</p></main>
       </div>
     );
   }
