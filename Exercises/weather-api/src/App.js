095413f4639dc3ab8570ce4391e15daa88366905
Bassel Kanso <basselkanso82@gmail.com>
2020-01-15 10:47:28 +0200

    making it a loop instead of hard coding it

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -7,6 +7,7 @@ import Weather from './weather';
 import FakeWeather from "./data/FakeWeather.json";
 
 const currentWeatherData = FakeWeather.list[0];
+const weatherData = FakeWeather.list;
 
 class App extends Component {
   constructor(props) {
@@ -25,6 +26,7 @@ class App extends Component {
   }
 
   render() {
+    let counter =0;
     return (
       <div className="app">
         <header className = "app__navbar">
@@ -33,14 +35,14 @@ class App extends Component {
         <main className="app__main">
           <CurrentWeather imgSrc={storm} description={currentWeatherData.weather[0].description} minTemp={this.toCelsius(currentWeatherData.main.temp_min)} maxTemp={this.toCelsius(currentWeatherData.main.temp_max)} humidity={currentWeatherData.main.humidity} pressure={currentWeatherData.main.pressure}/>
           <div className="app__main__weather-list">
-          <Weather time = {this.getTime(FakeWeather.list[1].dt_txt)} degree={this.toCelsius(FakeWeather.list[1].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[2].dt_txt)} degree={this.toCelsius(FakeWeather.list[2].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[3].dt_txt)} degree={this.toCelsius(FakeWeather.list[3].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[4].dt_txt)} degree={this.toCelsius(FakeWeather.list[4].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[5].dt_txt)} degree={this.toCelsius(FakeWeather.list[5].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[6].dt_txt)} degree={this.toCelsius(FakeWeather.list[6].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[7].dt_txt)} degree={this.toCelsius(FakeWeather.list[7].main.temp)} imgSrc={storm}/>
-          <Weather time = {this.getTime(FakeWeather.list[8].dt_txt)} degree={this.toCelsius(FakeWeather.list[8].main.temp)} imgSrc={storm}/>
+            { 
+            weatherData.map((element)=>{
+              counter ++;
+              if(counter <= 8){
+                return <Weather time = {this.getTime(element.dt_txt)} degree={this.toCelsius(element.main.temp)} imgSrc={storm}/>
+              }
+            },1)
+          }
           </div>
         </main>
       </div>
