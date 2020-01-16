0935c968d7448c9d324da94105691de13e488d49
Bassel Kanso <basselkanso82@gmail.com>
2020-01-16 12:47:15 +0200

    Creating Table

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -6,3 +6,6 @@
 "INSERT INTO students(Name,Age,Gender,Points) VALUES ("Bassel","20","M",500)"
 "UPDATE students SET points = 400 where Name = "Basma""
 "UPDATE students SET points = 150 where Name = "Alex""
+"INSERT INTO graduates (Name,Age,Gender,Points) VALUES ((SELECT Name FROM students where Name = "Layal"),(SELECT Age FROM students where Name = "Layal"),(SELECT Gender FROM students where Name = "Layal"),(SELECT Points FROM students where Name = "Layal"))"
+"UPDATE graduates SET Graduation = "16-1-2020" WHERE Name = "Layal""
+"DELETE FROM students WHERE Name = "Layal""
