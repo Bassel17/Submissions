e33034b45b69d50c43bf7ec2c8abdd73e964e3dd
Bassel Kanso <basselkanso82@gmail.com>
2020-01-16 10:03:01 +0200

    done with error handling

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from "react";
 import "./App.css";
-import storm from "./img/weather-icons/storm.svg";
 import Search from "./search";
 import CurrentWeather from "./currentWeather";
 import Weather from './weather';
-import FakeWeather from "./data/FakeWeather.json";
-
-const currentWeatherData = FakeWeather.list[0];
-const weatherData = FakeWeather.list;
+import clear from "./img/weather-icons/clear.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+import unknown from "./img/weather-icons/unknown.svg";
+import storm from "./img/weather-icons/storm.svg";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      weatherData:undefined,
+      city:"Beirut"
+    };
+  }
+
+  componentDidMount() {
+    this.recallApi(this.state.city);
+  }
+
+  async recallApi(city){
+    try{
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=6eb4331c142ff61ec60c501139b8cb96`);
+      const results = await response.json();
+      if(response.ok){
+        this.setState({
+          weatherData:results
+        });
+      }else{
+        alert("city not found");
+      }
+    }catch(e){
+      alert('Error: no connection');
+    }
+  }
+
+  getIconSrc = (id) => {
+    if(id<300){
+      return storm;
+    }else{
+      if(id>=300 && id <= 499){
+        return drizzle;
+      }else{ 
+        if(id>=500 && id <= 599){
+          return rain;
+        }else{
+          if(id>=600 && id <= 699){
+            return snow;
+          }else{
+            if(id>=700 && id <= 799){
+              return fog;
+            }else{
+              if(id === 800){
+                return clear;
+              }else{
+                if(id === 801){
+                  return partlycloudy;
+                }else{
+                  if(id>801 && id <= 805){
+                    return mostlycloudy;
+                  }else{
+                    return unknown;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   getTime = (fullTime) => {
@@ -21,32 +84,40 @@ class App extends Component {
     return `${newArray[0]}:${newArray[1]}`;
   }
 
-  toCelsius = (tempInKelvin)=>{
-    return Math.round(parseFloat(tempInKelvin) - 273.15);
+  toCelsius = (temp)=>{
+    return Math.round(parseFloat(temp));
+  }
+
+  callbackFunction = (childData)=>{
+    this.setState({city: childData});
+    this.recallApi(childData);
   }
 
   render() {
-    let counter =0;
-    return (
+    if(this.state.weatherData === undefined){
+      return(<div></div>);
+      }else{
+        const currentWeather = this.state.weatherData.list[0];
+      return (
       <div className="app">
         <header className = "app__navbar">
-            <Search value={this.value}/>
+            <Search value={this.state.city} parentCallback = {this.callbackFunction}/>
         </header>
         <main className="app__main">
-          <CurrentWeather imgSrc={storm} description={currentWeatherData.weather[0].description} minTemp={this.toCelsius(currentWeatherData.main.temp_min)} maxTemp={this.toCelsius(currentWeatherData.main.temp_max)} humidity={currentWeatherData.main.humidity} pressure={currentWeatherData.main.pressure}/>
+          <CurrentWeather imgSrc={this.getIconSrc(currentWeather.weather[0].id)} description={currentWeather.weather[0].description} minTemp={this.toCelsius(currentWeather.main.temp_min)} maxTemp={this.toCelsius(currentWeather.main.temp_max)} humidity={currentWeather.main.humidity} pressure={currentWeather.main.pressure}/>
           <div className="app__main__weather-list">
             { 
-            weatherData.map((element)=>{
-              counter ++;
-              if(counter <= 8){
-                return <Weather time = {this.getTime(element.dt_txt)} degree={this.toCelsius(element.main.temp)} imgSrc={storm}/>
-              }
-            },1)
+            this.state.weatherData.list.map((element,value)=>{
+              if(value > 0 && value<=7){
+              return <Weather key={value} time = {this.getTime(element.dt_txt)} degree={this.toCelsius(element.main.temp)} imgSrc={this.getIconSrc(element.weather[0].id)}/>
+            }
+          })
           }
           </div>
         </main>
       </div>
-    );
+      )
+    }
   }
 }
 
--- Exercises/weather-api/src/search.js
+++ Exercises/weather-api/src/search.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 class Search extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          searchInput:""
+        }
+    }
+    onChangeInput = (event) =>{
+        this.setState({
+          searchInput:event.target.value
+        });
+    }
+
+    sendSearchInput = () => {
+        this.props.parentCallback(this.state.searchInput);
+    }
     render(){
         return(
         <>
-            <input className="app__navbar__input" type="text" placeholder="Type in a city name" value={this.props.value}/>
-            <button className="app__navbar__button">Find Weather</button>
+            <input className="app__navbar__input" type="text" placeholder="Type in a city name" value={this.state.searchInput} onChange={this.onChangeInput}/>
+            <button className="app__navbar__button" onClick={event => {
+                this.sendSearchInput(this.state.searchInput);
+            }}>Find Weather</button>
         </>
         );
     }
