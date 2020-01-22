873872fbfd098467d7f9692251dab2a2727c0a50
Bassel Kanso <basselkanso82@gmail.com>
2020-01-22 14:11:49 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,6 +1,11 @@
+function run(test,message){
+    app.get(test, (req, res) => res.send(message));
+}
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send({message:'ok'}));
-
-app.listen(port);
+app.listen(port, ()=>console.log(`listing on port ${port}`));
+let date = new Date();
+let time = date.getHours() + ":" + date.getMinutes()
+run('/test',{status:200, message:"ok"});
+run('/time',{status:200, message:time});
