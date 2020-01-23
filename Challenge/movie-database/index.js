f29cb8d655977a929c1bf0c87fb8d99c561f94ac
Bassel Kanso <basselkanso82@gmail.com>
2020-01-23 10:17:44 +0200

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 app.listen(port, ()=>console.log(`listing on port ${port}`));
 app.get('/test', (req, res) =>res.send({status:200, message:"ok"}));
 let date = new Date();
@@ -14,3 +20,8 @@ app.get('/search',(req,res)=>{
     }
     res.send({status:200,message:'ok',data:req.query.s});
 });
+
+app.get('/movies/add',(req,res)=>res.send("add"));
+app.get('/movies/read',(req,res)=>res.send(movies));
+app.get('/movies/update',(req,res)=>res.send("update"));
+app.get('/movies/delete',(req,res)=>res.send("delete"));
