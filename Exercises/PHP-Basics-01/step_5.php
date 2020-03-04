5fac627c1967f117ed742d546b9d05b8d4a7749d
Bassel <basselkanso82@gmail.com>
2020-03-04 12:22:27 +0200

    Data types - lvl 1

---
+++ Exercises/PHP-Basics-01/step_5.php
@@ -0,0 +1,16 @@
+<?php
+$course_name="c++";
+$enrolled_students= 32;
+$price= 300.23;
+$on_discount=true;
+
+echo "$course_name \n";
+echo "$enrolled_students \n";
+echo "$price \n";
+if($on_discount){
+    echo "yes";
+}else{
+    echo "no";
+}
+
+?>
