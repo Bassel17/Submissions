1c09e8aa8985ca69d59188797050e43cea82ff5f
Bassel <basselkanso82@gmail.com>
2020-03-04 12:32:37 +0200

    PHP Control Statements -  If / else

---
+++ Exercises/PHP-Basics-01/Step_7.php
@@ -0,0 +1,15 @@
+<?php
+$birth_year=1997;
+$current_year=2020;
+$difference=$current_year - $birth_year;
+
+if($difference>=18){
+    echo "You can drive";
+}
+else 
+   echo "You still a kid, go and play GTA";
+
+
+
+
+?>
