28159b3587390a8875d5f90bd781ea56e009aaa4
Bassel <basselkanso82@gmail.com>
2020-03-04 12:16:05 +0200

    Strings Concatenation

---
+++ Exercises/PHP-Basics-01/Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name='Bassel';
+$position= 'Software Developer';
+$github_url='https://github.com/bassel-kanso';
+echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}"; 
+echo "\nHello, I'm ${name}" . " I'm a ${position}" . " please check my github link ${github_url}\n"; 
+echo $name . $position  .  $github_url ; 
+?>
