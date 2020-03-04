9267cee686563b8b81012cf37ec10ac8c9483ff8
Bassel <basselkanso82@gmail.com>
2020-03-04 12:37:20 +0200

    PHP Data types - Indexed Array

---
+++ Exercises/PHP-Basics-01/Step_8.php
@@ -0,0 +1,8 @@
+<?php
+
+$groceries = ["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+
+echo "Hello Sir, do you have ${groceries[0]}, ${groceries[1]}, and ${groceries[2]}? Also if you sell fruits can I find a ${groceries[5]}?";
+
+
+?>
